cdb191ce38e7778b840d4f5272fd03b94a0116e5
Da11oS <4eduma4@gmail.com>
2020-11-22 20:19:11 +0300

    Add more code

--- ОСИС_11ЛБ Файловые операции/Form1.Designer.cs
+++ ОСИС_11ЛБ Файловые операции/Form1.Designer.cs
@@ -30,39 +30,209 @@
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.FileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.FolderButton = new System.Windows.Forms.Button();
+            this.WriteUnitButton = new System.Windows.Forms.Button();
+            this.UnitReadButton = new System.Windows.Forms.Button();
+            this.RadLineButton = new System.Windows.Forms.Button();
+            this.CloseForReadButton = new System.Windows.Forms.Button();
+            this.OpenForReadingButton = new System.Windows.Forms.Button();
+            this.FindButton = new System.Windows.Forms.Button();
+            this.WriteLineButton = new System.Windows.Forms.Button();
+            this.CloseForWriteButton = new System.Windows.Forms.Button();
+            this.OpenForWriteButton = new System.Windows.Forms.Button();
+            this.FileTextBox = new System.Windows.Forms.TextBox();
+            this.PositionTextBox = new System.Windows.Forms.TextBox();
+            this.ForFileLabel = new System.Windows.Forms.Label();
+            this.PositionLabel = new System.Windows.Forms.Label();
+            this.FunctionToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(67, 192);
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 310);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(361, 74);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1});
+            this.FileToolStripMenuItem1,
+            this.FunctionToolStripMenuItem2});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Size = new System.Drawing.Size(361, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
-            // toolStripMenuItem1
+            // FileToolStripMenuItem1
             // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(125, 20);
-            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            this.FileToolStripMenuItem1.Name = "FileToolStripMenuItem1";
+            this.FileToolStripMenuItem1.Size = new System.Drawing.Size(48, 20);
+            this.FileToolStripMenuItem1.Text = "Файл";
+            // 
+            // FolderButton
+            // 
+            this.FolderButton.Location = new System.Drawing.Point(102, 40);
+            this.FolderButton.Name = "FolderButton";
+            this.FolderButton.Size = new System.Drawing.Size(166, 23);
+            this.FolderButton.TabIndex = 2;
+            this.FolderButton.Text = "Папка";
+            this.FolderButton.UseVisualStyleBackColor = true;
+            this.FolderButton.Click += new System.EventHandler(this.FolderButton_Click);
+            // 
+            // WriteUnitButton
+            // 
+            this.WriteUnitButton.Location = new System.Drawing.Point(183, 227);
+            this.WriteUnitButton.Name = "WriteUnitButton";
+            this.WriteUnitButton.Size = new System.Drawing.Size(141, 23);
+            this.WriteUnitButton.TabIndex = 3;
+            this.WriteUnitButton.Text = "Запись блока";
+            this.WriteUnitButton.UseVisualStyleBackColor = true;
+            this.WriteUnitButton.Click += new System.EventHandler(this.WriteUnitButton_Click);
+            // 
+            // UnitReadButton
+            // 
+            this.UnitReadButton.Location = new System.Drawing.Point(39, 227);
+            this.UnitReadButton.Name = "UnitReadButton";
+            this.UnitReadButton.Size = new System.Drawing.Size(138, 23);
+            this.UnitReadButton.TabIndex = 3;
+            this.UnitReadButton.Text = "Чтение блока";
+            this.UnitReadButton.UseVisualStyleBackColor = true;
+            this.UnitReadButton.Click += new System.EventHandler(this.UnitReadButton_Click);
+            // 
+            // RadLineButton
+            // 
+            this.RadLineButton.Location = new System.Drawing.Point(184, 69);
+            this.RadLineButton.Name = "RadLineButton";
+            this.RadLineButton.Size = new System.Drawing.Size(165, 23);
+            this.RadLineButton.TabIndex = 3;
+            this.RadLineButton.Text = "Чтение строки";
+            this.RadLineButton.UseVisualStyleBackColor = true;
+            this.RadLineButton.Click += new System.EventHandler(this.RadLineButton_Click);
+            // 
+            // CloseForReadButton
+            // 
+            this.CloseForReadButton.Location = new System.Drawing.Point(183, 127);
+            this.CloseForReadButton.Name = "CloseForReadButton";
+            this.CloseForReadButton.Size = new System.Drawing.Size(166, 23);
+            this.CloseForReadButton.TabIndex = 3;
+            this.CloseForReadButton.Text = "Закрыть для чтения";
+            this.CloseForReadButton.UseVisualStyleBackColor = true;
+            this.CloseForReadButton.Click += new System.EventHandler(this.CloseForReadButton_Click);
+            // 
+            // OpenForReadingButton
+            // 
+            this.OpenForReadingButton.Location = new System.Drawing.Point(13, 69);
+            this.OpenForReadingButton.Name = "OpenForReadingButton";
+            this.OpenForReadingButton.Size = new System.Drawing.Size(165, 23);
+            this.OpenForReadingButton.TabIndex = 3;
+            this.OpenForReadingButton.Text = "Открыть для чтения";
+            this.OpenForReadingButton.UseVisualStyleBackColor = true;
+            this.OpenForReadingButton.Click += new System.EventHandler(this.OpenForReadingButton_Click);
+            // 
+            // FindButton
+            // 
+            this.FindButton.Location = new System.Drawing.Point(136, 256);
+            this.FindButton.Name = "FindButton";
+            this.FindButton.Size = new System.Drawing.Size(75, 23);
+            this.FindButton.TabIndex = 3;
+            this.FindButton.Text = "Найти";
+            this.FindButton.UseVisualStyleBackColor = true;
+            this.FindButton.Click += new System.EventHandler(this.FindButton_Click);
+            // 
+            // WriteLineButton
+            // 
+            this.WriteLineButton.Location = new System.Drawing.Point(13, 127);
+            this.WriteLineButton.Name = "WriteLineButton";
+            this.WriteLineButton.Size = new System.Drawing.Size(164, 23);
+            this.WriteLineButton.TabIndex = 3;
+            this.WriteLineButton.Text = "Запись строки";
+            this.WriteLineButton.UseVisualStyleBackColor = true;
+            this.WriteLineButton.Click += new System.EventHandler(this.WriteLineButton_Click);
+            // 
+            // CloseForWriteButton
+            // 
+            this.CloseForWriteButton.Location = new System.Drawing.Point(184, 98);
+            this.CloseForWriteButton.Name = "CloseForWriteButton";
+            this.CloseForWriteButton.Size = new System.Drawing.Size(165, 23);
+            this.CloseForWriteButton.TabIndex = 3;
+            this.CloseForWriteButton.Text = "Закрыть для записи";
+            this.CloseForWriteButton.UseVisualStyleBackColor = true;
+            this.CloseForWriteButton.Click += new System.EventHandler(this.CloseForWriteButton_Click);
+            // 
+            // OpenForWriteButton
+            // 
+            this.OpenForWriteButton.Location = new System.Drawing.Point(12, 98);
+            this.OpenForWriteButton.Name = "OpenForWriteButton";
+            this.OpenForWriteButton.Size = new System.Drawing.Size(166, 23);
+            this.OpenForWriteButton.TabIndex = 3;
+            this.OpenForWriteButton.Text = "Открыть для записи";
+            this.OpenForWriteButton.UseVisualStyleBackColor = true;
+            this.OpenForWriteButton.Click += new System.EventHandler(this.OpenForWriteButton_Click);
+            // 
+            // FileTextBox
+            // 
+            this.FileTextBox.Location = new System.Drawing.Point(77, 201);
+            this.FileTextBox.Name = "FileTextBox";
+            this.FileTextBox.Size = new System.Drawing.Size(100, 20);
+            this.FileTextBox.TabIndex = 4;
+            // 
+            // PositionTextBox
+            // 
+            this.PositionTextBox.Location = new System.Drawing.Point(183, 201);
+            this.PositionTextBox.Name = "PositionTextBox";
+            this.PositionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PositionTextBox.TabIndex = 4;
+            // 
+            // ForFileLabel
+            // 
+            this.ForFileLabel.AutoSize = true;
+            this.ForFileLabel.Location = new System.Drawing.Point(102, 174);
+            this.ForFileLabel.Name = "ForFileLabel";
+            this.ForFileLabel.Size = new System.Drawing.Size(63, 13);
+            this.ForFileLabel.TabIndex = 5;
+            this.ForFileLabel.Text = "Для файла";
+            // 
+            // PositionLabel
+            // 
+            this.PositionLabel.AutoSize = true;
+            this.PositionLabel.Location = new System.Drawing.Point(205, 174);
+            this.PositionLabel.Name = "PositionLabel";
+            this.PositionLabel.Size = new System.Drawing.Size(51, 13);
+            this.PositionLabel.TabIndex = 5;
+            this.PositionLabel.Text = "Позиция";
+            // 
+            // FunctionToolStripMenuItem2
+            // 
+            this.FunctionToolStripMenuItem2.Name = "FunctionToolStripMenuItem2";
+            this.FunctionToolStripMenuItem2.Size = new System.Drawing.Size(68, 20);
+            this.FunctionToolStripMenuItem2.Text = "Функции";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(361, 354);
+            this.ClientSize = new System.Drawing.Size(361, 384);
+            this.Controls.Add(this.PositionLabel);
+            this.Controls.Add(this.ForFileLabel);
+            this.Controls.Add(this.PositionTextBox);
+            this.Controls.Add(this.FileTextBox);
+            this.Controls.Add(this.OpenForWriteButton);
+            this.Controls.Add(this.CloseForReadButton);
+            this.Controls.Add(this.CloseForWriteButton);
+            this.Controls.Add(this.OpenForReadingButton);
+            this.Controls.Add(this.WriteLineButton);
+            this.Controls.Add(this.RadLineButton);
+            this.Controls.Add(this.UnitReadButton);
+            this.Controls.Add(this.FindButton);
+            this.Controls.Add(this.WriteUnitButton);
+            this.Controls.Add(this.FolderButton);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -79,7 +249,22 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem FunctionToolStripMenuItem2;
+        private System.Windows.Forms.Button FolderButton;
+        private System.Windows.Forms.Button WriteUnitButton;
+        private System.Windows.Forms.Button UnitReadButton;
+        private System.Windows.Forms.Button RadLineButton;
+        private System.Windows.Forms.Button CloseForReadButton;
+        private System.Windows.Forms.Button OpenForReadingButton;
+        private System.Windows.Forms.Button FindButton;
+        private System.Windows.Forms.Button WriteLineButton;
+        private System.Windows.Forms.Button CloseForWriteButton;
+        private System.Windows.Forms.Button OpenForWriteButton;
+        private System.Windows.Forms.TextBox FileTextBox;
+        private System.Windows.Forms.TextBox PositionTextBox;
+        private System.Windows.Forms.Label ForFileLabel;
+        private System.Windows.Forms.Label PositionLabel;
     }
 }
 
--- ОСИС_11ЛБ Файловые операции/Form1.cs
+++ ОСИС_11ЛБ Файловые операции/Form1.cs
@@ -7,14 +7,188 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ОСИС_11ЛБ_Файловые_операции
 {
     public partial class Form1 : Form
     {
+        int n5 = 10;//добавить № варианта
+        FileStream file1;
+        StreamReader reader;
+        string filename1;
+        StreamWriter writer;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void FolderButton_Click(object sender, EventArgs e)
+        {
+            FileInfo fsi = new FileInfo(openFileDialog1.FileName);
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            DirectoryInfo di = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            // Get a reference to each file in that directory.
+            FileInfo[] fiArr = di.GetFiles();
+            // Display the names and sizes of the files.
+            richTextBox1.AppendText("\n" + "The directory contains the following files:" + di.Name);
+            foreach (FileInfo f in fiArr)
+                richTextBox1.AppendText("\n" + "The size of " + f.Name + " is " + f.Length + " bytes. " + f.CreationTime);
+
+        }
+
+        private void FindButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Find(textBox1.Text);
+            richTextBox1.SelectionColor = Color.Red;
+
+        }
+
+        private void OpenForReadingButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл            
+            filename1 = openFileDialog1.FileName;
+            file1 = new FileStream(filename1, FileMode.Open);
+            reader = new StreamReader(file1);
+
+        }
+
+        private void RadLineButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText("\n" + reader.ReadLine());
+        }
+
+        private void WriteLineButton_Click(object sender, EventArgs e)
+        {
+            writer.WriteLine(textBox1.Text);
+        }
+
+        private void CloseForReadButton_Click(object sender, EventArgs e)
+        {
+            reader.Close();
+        }
+
+        private void OpenForWriteButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл            
+            filename1 = openFileDialog1.FileName;
+            file1 = new FileStream(filename1, FileMode.Append);
+            writer = new StreamWriter(file1);
+
+        }
+
+        private void CloseForWriteButton_Click(object sender, EventArgs e)
+        {
+            writer.Close();
+        }
+
+        private void UnitReadButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл            
+            string filename = openFileDialog1.FileName;
+            using (FileStream fstream = new FileStream(@filename, FileMode.OpenOrCreate))
+            {
+
+                fstream.Seek(int.Parse(textBox2.Text) + 2, SeekOrigin.Begin);//  .End);
+                byte[] output = new byte[n5];
+                int n = fstream.Read(output, 0, output.Length);
+                string textFromFile = Encoding.Default.GetString(output);
+                textBox1.Text = textFromFile;
+
+            }
+        }
+
+        private void WriteUnitButton_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл            
+            string filename = saveFileDialog1.FileName;
+            using (FileStream fstream = new FileStream(@filename, FileMode.OpenOrCreate))
+            {
+                string replaceText = textBox1.Text;
+                if (replaceText.Length < n5)
+                {
+                    replaceText = replaceText + "..........";
+                }
+                if (replaceText.Length > n5)
+                {
+                    replaceText = replaceText.Substring(0, n5);// обрезаем сначала до 5 символов
+                }
+                fstream.Seek(int.Parse(textBox2.Text) + 2, SeekOrigin.Begin); // на 5 симво-лов 
+                byte[] input = Encoding.Default.GetBytes(replaceText);
+                fstream.Write(input, 0, input.Length);
+            }
+
+        }
+
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            // использовать расширение по варианту
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл            
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+
+            string fileText = System.IO.File.ReadAllText(filename);
+            richTextBox1.Text = fileText;
+            string s1 = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+            string s2 = "abvgde_=zijklmnoprstufhc@%&*y()-!ABVGDEEJZIJKLMNOPRSTUFHCHSITYWLOY";
+            string s = "";
+            string buf = richTextBox1.Text;
+            int k = 0;
+            for (int i = 0; i < buf.Length; i++)
+            {
+                k = 0;
+                for (int j = 1; j < 66; j++)
+                {
+                    if (s1[j] == buf[i])
+                    { k = j; };
+                };
+                if (k > 0)
+                { s = s + s2[k].ToString(); }
+                else
+                { s = s + buf[i].ToString(); };
+            };
+            System.IO.File.WriteAllText(filename, s, Encoding.UTF8);
+        }
+
+        private void создатьПапкуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            Directory.SetCurrentDirectory(folderBrowserDialog1.SelectedPath);
+            Directory.CreateDirectory("TestDir");     //№ варианта       
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            FileInfo fsi = new FileInfo(openFileDialog1.FileName);
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            DirectoryInfo di = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            // Get a reference to each file in that directory.
+            FileInfo[] fiArr = di.GetFiles();
+            // Display the names and sizes of the files.
+            richTextBox1.AppendText("\n" + "The directory contains the following files:" + di.Name);
+            foreach (FileInfo f in fiArr)
+                richTextBox1.AppendText("\n" + "The size of " + f.Name + " is " + f.Length + " bytes. " + f.CreationTime);
+        }
+
+
     }
 }
